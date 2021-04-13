66e0c29c1de41fa4678f32906cbdd15ca5513903
C012496 <C012496@GR6N5H2.galileo.corp.lcl>
2021-04-13 16:49:15 +0200

    Full pipeline test

--- src/net/codejava/javaee/HelloServlet.java
+++ src/net/codejava/javaee/HelloServlet.java
@@ -33,7 +33,7 @@ public class HelloServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		String yourName = request.getParameter("yourName");
 		PrintWriter writer = response.getWriter();
-		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.println("<h1>Hello Big .... " + yourName + "</h1>");
 		writer.close();
 		// doGet(request, response);
 	}
